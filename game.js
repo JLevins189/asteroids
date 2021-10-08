176fbcd34148e3ed53931c23325b83a012cd6275
JLev189 <jacklevins@hotmail.com>
2021-10-08 12:32:26 +0100

    Fixed the bug that fires serveral bullets at a time

--- game.js
+++ game.js
@@ -390,7 +390,7 @@ Ship = function () {
                                0, 11,
                                3,  6]);
 
-  this.delayBeforeBullet = 0;
+  this.delayBeforeBullet = 22;
 
   this.postMove = this.wrapPostMove;
 
@@ -421,6 +421,7 @@ Ship = function () {
     }
     if (KEY_STATUS.space) {
       if (this.delayBeforeBullet <= 0) {
+		  this.delayBeforeBullet = 10;
         for (var i = 0; i < this.bullets.length; i++) {
           if (!this.bullets[i].visible) {
             SFX.laser();
@@ -492,7 +493,7 @@ BigAlien = function () {
   this.bridgesH = false;
 
   this.bullets = [];
-  this.delayBeforeBullet = 0;
+  this.delayBeforeBullet = 22;
 
   this.newPosition = function () {
     if (Math.random() < 0.5) {
